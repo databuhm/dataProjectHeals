4197dcdc09d4a0db1ac121e0caf05099ecf49611
databuhm <databuhm@gmail.com>
2024-10-08 21:07:48 +0900

    make new modules

--- dataLoader/makeConverters.py
+++ dataLoader/makeConverters.py
@@ -25,3 +25,28 @@ def csvWithChunks(csvFile, chunkSize=100000):
     print("Running: ", str(datetime.timedelta(seconds=(time.time() - start))).split(".")[0])
     
     return convDict
+
+def sasWithChunks(sasFile, chunkSize=100000):
+    import pyreadstat, datetime, time
+
+    print("Start: ", datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S'))
+    start = time.time()
+
+    convDict = {}
+    
+    for df, meta in pyreadstat.read_file_in_chunks(pyreadstat.read_sas7bdat, sasFile, chunksize=chunkSize):
+        for col in df.columns:
+            if col in convDict:
+                continue
+            if df[col].dtype == 'object':
+                try:
+                    df[col].astype(float)
+                except ValueError:
+                    convDict[col] = str
+
+    print("Converters=convDict:", {k: v.__name__ for k, v in convDict.items()})
+
+    print("End: ", datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S'))
+    print("Running: ", str(datetime.timedelta(seconds=(time.time() - start))).split(".")[0])
+    
+    return convDict
---
+++ dataLoader/makeDataFrame.py
@@ -0,0 +1,39 @@
+def makeCsvDataFrame(csvFile):
+    import pandas as pd
+    from dataLoader.makeConverters import csvWithChunks
+    
+    convDict = csvWithChunks(csvFile)
+    df = pd.read_csv(csvFile, converters=convDict)
+    
+    return df
+
+def makeSasDataFrame(sasFile, chunkSize=100000):
+    import pyreadstat
+    from dataLoader.makeConverters import sasWithChunks
+    
+    convDict = sasWithChunks(sasFile, chunkSize)
+    df, meta = pyreadstat.read_sas7bdat(sasFile)
+    
+    for col, dtype in convDict.items():
+        df[col] = df[col].astype(dtype)
+
+    return df
+
+def makeVariousSasDataFrame(sasFolderPath) -> dict: 
+    import os
+    
+    dfDict = {}
+    sasList = sorted([file for file in os.listdir(sasFolderPath) if file.endswith('.sas7bdat')])
+    
+    for idx, sasFile in enumerate(sasList):
+        # fileName = os.path.splitext(sasFile)[0]
+        dfName = f"{os.path.splitext(sasFile)[0]}_{idx}"
+        # dfName = f"{fileName.split('_')[0]}_{idx}"
+        
+        sasFilePath = os.path.join(sasFolderPath, sasFile)
+        df = makeSasDataFrame(sasFilePath)
+        
+        dfDict[dfName] = df
+        print(f"make {dfName} is Ready: {df.shape}")
+
+    return dfDict
